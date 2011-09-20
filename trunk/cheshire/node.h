5fc4ee77794bb916ec8c2cb261bfae937d400e3f
gimenez.tom@gmail.com <gimenez.tom@gmail.com@3040dc66-f2c5-6624-7679-62bdbddada0d>
2011-09-20 11:20:22 +0000

    Changement de Node vers node

---
+++ trunk/cheshire/node.h
@@ -0,0 +1,26 @@
+#ifndef __Node__
+#define __Node__
+
+// Include Vector class
+#include "vector.h"
+#include "ray.h"
+#include "Intersection.h"
+
+class Node
+{
+public:
+	Node(void);
+	virtual ~Node(void);
+
+	virtual int Intersect( const Ray &, Intersection&) =0;
+	//virtual int Intersect( const Ray &, double &, double & ) =0;
+	virtual int PMC( const Vector& ) =0;
+
+	virtual Vector getPosition() =0;
+	virtual Vector getEmission() =0;
+	virtual Vector getColor() =0;
+	virtual double getF() =0;
+	virtual int getRefl() =0;
+};
+
+#endif
