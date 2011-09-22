8f21fd09da3df0e323d319e923479cefa916c207
gimenez.tom@gmail.com <gimenez.tom@gmail.com@3040dc66-f2c5-6624-7679-62bdbddada0d>
2011-09-22 16:08:15 +0000

    Ajout de test dans exe Modif de Box pour que la normale soit retournée correctement

--- sources/exe.cpp
+++ sources/exe.cpp
@@ -1,5 +1,5 @@
-#include <math.h>   
-#include <stdlib.h> 
+#include <math.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <ctime>
 #include <vector>
@@ -25,7 +25,7 @@
 inline double Random() { return (rand()%10000)/9999.0;}
 
 //! build a coordinate system from vector v1
-inline 
+inline
 void CoordinateSystem( const Vector &v1, Vector *v2, Vector *v3 )
 {
 	if ( fabsf( v1[0] ) > fabsf( v1[1] ) )
@@ -38,56 +38,56 @@ void CoordinateSystem( const Vector &v1, Vector *v2, Vector *v3 )
         float invLen = 1.f / sqrtf( v1[1] * v1[1] + v1[2] * v1[2] );
         *v2 = Vector( 0.f, v1[2] * invLen, -v1[1] * invLen );
     }
-    
+
     *v3 =  v1 / *v2 ;
 }
 
-//!< Scene defined as an array of nodes (radius, position, emission, color, material) 
+//!< Scene defined as an array of nodes (radius, position, emission, color, material)
 std::vector<Node *> nodes;
 int nbObj;
 
 /*!
 \brief Clamp value to unity.
 */
-inline double clamp(double x){ return x<0 ? 0 : x>1 ? 1 : x; } 
+inline double clamp(double x){ return x<0 ? 0 : x>1 ? 1 : x; }
 
 /*!
 \brief Convert to int and apply a gamma power of 2.2.
 */
-inline int toInt(double x){ return int(pow(clamp(x),1/2.2)*255+.5); } 
+inline int toInt(double x){ return int(pow(clamp(x),1/2.2)*255+.5); }
 
 /*!
 \brief Compute the intersection between the ray and the scene.
 \brief Return true if an intersection is found and the nearest object found
 */
 inline bool IntersectScene(const Ray &r, Intersection& inter)
-{ 
+{
 	Intersection tempInter;
 	bool k=false;
 	// for each sphere
-	for (int i=0;i<nbObj;i++) 
+	for (int i=0;i<nbObj;i++)
 	{
-		double d; 
+		double d;
 		// if the ray intersects the sphere i
-		if(nodes[i]->Intersect(r,tempInter)) 
-		{ 
+		if(nodes[i]->Intersect(r,tempInter))
+		{
 			// if it's the first intersection
-			if (k==false) { 
+			if (k==false) {
 				inter = tempInter;
 				d = tempInter.t;
-			} 
+			}
 			// else if the new intersection is in front of the last registered
-			else { 
+			else {
 				if (d>tempInter.t) {
 					inter=tempInter;
 					d=tempInter.t;
-				} 
-			} 
+				}
+			}
 			k=true;
 		}
 	}
-	return k; 
-} 
+	return k;
+}
 #define M_PI 3.141592654
 
 /*!
@@ -96,13 +96,13 @@ inline bool IntersectScene(const Ray &r, Intersection& inter)
 \brief depth Recursion depth.
 */
 Vector radiance(const Ray &r, int depth)
-{ 
-	Intersection inter;                     // id of Intersected object 
+{
+	Intersection inter;                     // id of Intersected object
 	// if no intersection found, return the null vector
-	if (!IntersectScene(r, inter)) return Vector(0.0,0.0,0.0); 
+	if (!IntersectScene(r, inter)) return Vector(0.0,0.0,0.0);
 
 	Node * obj = inter.obj;
-	
+
 	//intersection coordinates
 	Vector x = inter.pos;
 	//normal of the object at the intersection
@@ -114,39 +114,39 @@ Vector radiance(const Ray &r, int depth)
 	double p=obj->getF();
 
 	// if no rebound is required
-    if (++depth>D) /*if (Random()<p) f=f*(1/p); else*/ return obj->getEmission(); //R.R. 
+    if (++depth>D) /*if (Random()<p) f=f*(1/p); else*/ return obj->getEmission(); //R.R.
     if (obj->getRefl() == 0) // Diffuse
-    {    
-            double r1=2*M_PI*Random(), r2=Random(), r2s=sqrt(r2); 
+    {
+            double r1=2*M_PI*Random(), r2=Random(), r2s=sqrt(r2);
             Vector w=nl;
             Vector u;
             Vector v;
 
 			CoordinateSystem(w,&u,&v);
-            Vector d = Normalized(u*cos(r1)*r2s + v*sin(r1)*r2s + w*sqrt(1-r2)); 
-            return obj->getEmission() + f.Scale(radiance(Ray(x,d),depth)); 
-    } 
+            Vector d = Normalized(u*cos(r1)*r2s + v*sin(r1)*r2s + w*sqrt(1-r2));
+            return obj->getEmission() + f.Scale(radiance(Ray(x,d),depth));
+    }
     else if (obj->getRefl() == 1)  // Specular
     {
-            return obj->getEmission() + f.Scale(radiance(Ray(x,r.Direction()-n*2*(n*r.Direction())),depth)); 
+            return obj->getEmission() + f.Scale(radiance(Ray(x,r.Direction()-n*2*(n*r.Direction())),depth));
     }
     else // Refractive
     {
-            Ray reflRay(x, r.Direction()-n*2*n*(r.Direction()));    
-            bool into = n*nl>0;                // Ray from outside going in? 
-            double nc=1, nt=1.5, nnt=into?nc/nt:nt/nc, ddn=r.Direction()*nl, cos2t; 
-            if ((cos2t=1-nnt*nnt*(1-ddn*ddn))<0)    // Total internal reflection 
-                    return obj->getEmission() + f.Scale(radiance(reflRay,depth)); 
-            Vector tdir = Normalized(r.Direction()*nnt - n*((into?1:-1)*(ddn*nnt+sqrt(cos2t)))); 
-            double a=nt-nc, b=nt+nc, R0=a*a/(b*b), c = 1-(into?-ddn:tdir*n); 
-            double Re=R0+(1-R0)*c*c*c*c*c,Tr=1-Re,P=.25+.5*Re,RP=Re/P,TP=Tr/(1-P); 
+            Ray reflRay(x, r.Direction()-n*2*n*(r.Direction()));
+            bool into = n*nl>0;                // Ray from outside going in?
+            double nc=1, nt=1.5, nnt=into?nc/nt:nt/nc, ddn=r.Direction()*nl, cos2t;
+            if ((cos2t=1-nnt*nnt*(1-ddn*ddn))<0)    // Total internal reflection
+                    return obj->getEmission() + f.Scale(radiance(reflRay,depth));
+            Vector tdir = Normalized(r.Direction()*nnt - n*((into?1:-1)*(ddn*nnt+sqrt(cos2t))));
+            double a=nt-nc, b=nt+nc, R0=a*a/(b*b), c = 1-(into?-ddn:tdir*n);
+            double Re=R0+(1-R0)*c*c*c*c*c,Tr=1-Re,P=.25+.5*Re,RP=Re/P,TP=Tr/(1-P);
 
-            return obj->getEmission() + f.Scale(depth>2 ? (Random()<P ?   // Russian roulette 
-                    radiance(reflRay,depth)*RP:radiance(Ray(x,tdir),depth)*TP) : 
-            radiance(reflRay,depth)*Re+radiance(Ray(x,tdir),depth)*Tr); 
+            return obj->getEmission() + f.Scale(depth>2 ? (Random()<P ?   // Russian roulette
+                    radiance(reflRay,depth)*RP:radiance(Ray(x,tdir),depth)*TP) :
+            radiance(reflRay,depth)*Re+radiance(Ray(x,tdir),depth)*Tr);
     }
 
-} 
+}
 
 double now()
 {
@@ -155,7 +155,7 @@ double now()
 }
 
 int main()
-{ 
+{
 
 	Sphere * sp1 = new Sphere(15, Vector( 35,40.8,82.6), Vector(0.0,0.0,0.0),Vector(.75,.25,.25),0);
 	Sphere * sp2 = new Sphere(15, Vector( 25,40.8,82.6), Vector(0.0,0.0,0.0),Vector(.25,.25,.75),0);
@@ -163,46 +163,61 @@ int main()
 	//nodes.push_back(new Inter( sp1, sp2));
 	nodes.push_back(new Sphere(1e5, Vector( 1e5+1,40.8,81.6), Vector(0.0,0.0,0.0),Vector(.75,.25,.25),0));//Left red
 	nodes.push_back(new Sphere(1e5, Vector(-1e5+99,40.8,81.6),Vector(0.0,0.0,0.0),Vector(.25,.25,.75),0));//Right blue
-	nodes.push_back(new Sphere(1e5, Vector(50,40.8, 1e5),     Vector(0.0,0.0,0.0),Vector(.75,.75,.75),0));//Back 
-	//nodes.push_back(new Sphere(1e5, Vector(50,40.8,-1e5+170), Vector(0.0,0.0,0.0),Vector(0.0,0.0,0.0),0));//Front 
+	nodes.push_back(new Sphere(1e5, Vector(50,40.8, 1e5),     Vector(0.0,0.0,0.0),Vector(.75,.75,.75),0));//Back
+	//nodes.push_back(new Sphere(1e5, Vector(50,40.8,-1e5+170), Vector(0.0,0.0,0.0),Vector(0.0,0.0,0.0),0));//Front
 	//nodes.push_back(new Sphere(1e5, Vector(50, 1e5, 81.6),    Vector(0.0,0.0,0.0),Vector(.75,.75,.75),0));//Botom white
 	nodes.push_back(new Sphere(1e5, Vector(50,-1e5+81.6,81.6),Vector(0.0,0.0,0.0),Vector(.75,.75,.75),0));//Top white
-	//nodes.push_back(new Sphere(16.5,Vector(27,16.5,47),       Vector(0.0,0.0,0.0),Vector(1,1,1)*.999,1));//Mirror 
-	//nodes.push_back(new Sphere(16.5,Vector(73,16.5,78),       Vector(0.0,0.0,0.0),Vector(1,1,1)*.999, 2));//Glass 
+	//nodes.push_back(new Sphere(16.5,Vector(27,16.5,47),       Vector(0.0,0.0,0.0),Vector(1,1,1)*.999,1));//Mirror
+	//nodes.push_back(new Sphere(16.5,Vector(73,16.5,78),       Vector(0.0,0.0,0.0),Vector(1,1,1)*.999, 2));//Glass
 	nodes.push_back(new Sphere(600, Vector(50,681.6-.27,81.6),Vector(12,12,12),  Vector(0.0,0.0,0.0), 0));//Light
-	nodes.push_back(new Box(Vector(42,22.5,100), Vector(55,29,120), Vector(0.0,0.0,0.0), Vector(0.75,0.25,0.25), 0)); //First box 
+	//nodes.push_back(new Box(Vector(42,22.5,100), Vector(55,29,120), Vector(0.0,0.0,0.0), Vector(0.75,0.25,0.25), 0)); //First box
+	nodes.push_back(new Box(Vector(0,0,0), Vector(55,29,120), Vector(0.0,0.0,0.0), Vector(0.75,0.25,0.25), 0)); //First box
 
 	nbObj = (int) nodes.size();
 
 
-	Box b = Box(Vector(0,2,-1), Vector(6,4,1), Vector(0.0,0.0,0.0), Vector(0.75,0.25,0.25), 0);
-	Ray r = Ray(Vector(1,0,0), Vector(1,1,0));
+	Box b = Box(Vector(0,2,1), Vector(6,4,-1), Vector(0.0,0.0,0.0), Vector(0.75,0.25,0.25), 0);
+	Ray r1 = Ray(Vector(0,0,0), Vector(1,1,0)); //impact dessous
+	Ray r2 = Ray(Vector(0,6,0), Vector(1,-1,0)); //impact dessus
+	Ray r3 = Ray(Vector(8,1,0), Vector(-1,1,0)); //impact dessus
+	Ray r4 = Ray(Vector(-2,1,0), Vector(1,1,0)); //impact dessus
+	Ray r5 = Ray(Vector(-2,1,0), Vector(1,1,0)); //impact dessus
 
 	Intersection i;
-	b.Intersect(r,i);
 
-	fprintf(stderr,"pos %f %f %f \n",i.pos[0],i.pos[1],i.pos[2]);
-	fprintf(stderr,"normal %f %f %f \n",i.normal[0],i.normal[1],i.normal[2]);
+	b.Intersect(r1,i);
+	fprintf(stderr,"pour r1: pos %f %f %f \n",i.pos[0],i.pos[1],i.pos[2]);
+	fprintf(stderr,"pour r1: normal %f %f %f \n",i.normal[0],i.normal[1],i.normal[2]);
 
+	b.Intersect(r2,i);
+	fprintf(stderr,"pour r2: pos %f %f %f \n",i.pos[0],i.pos[1],i.pos[2]);
+	fprintf(stderr,"pour r2: normal %f %f %f \n",i.normal[0],i.normal[1],i.normal[2]);
 
+	b.Intersect(r3,i);
+	fprintf(stderr,"pour r3: pos %f %f %f \n",i.pos[0],i.pos[1],i.pos[2]);
+	fprintf(stderr,"pour r3: normal %f %f %f \n",i.normal[0],i.normal[1],i.normal[2]);
 
+	b.Intersect(r4,i);
+	fprintf(stderr,"pour r4: pos %f %f %f \n",i.pos[0],i.pos[1],i.pos[2]);
+	fprintf(stderr,"pour r4: normal %f %f %f \n",i.normal[0],i.normal[1],i.normal[2]);
 
 
 	double t = now();
 	//fprintf(stderr, " %f %f %f \n \n",nodes[9]->getColor()[0],nodes[9]->getColor()[1],nodes[9]->getColor()[2]);
-	int w=X; 
-	int h=Y; 
+	int w=X;
+	int h=Y;
 	int samps = S;
 	// Save the color of each pixel
-	Vector* c = new Vector[X*Y]; 
-	
-	Ray camera(Vector(50,52,295.6), Normalized(Vector(0,-0.042612,-1))); // camera pos, dir 
+	Vector* c = new Vector[X*Y];
+
+	Ray camera(Vector(50,52,295.6), Normalized(Vector(0,-0.042612,-1))); // camera pos, dir
 	Vector cx=Vector(w*.5135/h,0,0), cy=Normalized(cx/camera.Direction())*.5135;
 
 	// for each line of pixel
 	#pragma omp parallel for num_threads(omp_get_num_procs())
+/*	for (int y=0; y<0; y++)*/
 	for (int y=0; y<h; y++)
-	{  
+	{
 		//progression update
 		fprintf(stderr,"\rRendering (%d spp) %5.2f%%",samps*(Pixel*Pixel),100.*y/(h-1));
 		// for each column of pixel
@@ -215,36 +230,36 @@ int main()
 			// the focused pixel is subdivided in 4 ( 2x2 )
 			for (int sy=0; sy<Pixel; sy++)
 			{
-				// 2x2 subpixel rows 
+				// 2x2 subpixel rows
 				for (int sx=0; sx<Pixel; sx++)
 				{
 					Vector r(0.0,0.0,0.0);
 					// in each part of the pixel, $samps ray are thrown
 					for (int s=0; s<samps; s++)
-					{ 
-						double r1=2*Random(), dx=r1<1 ? sqrt(r1)-1: 1-sqrt(2-r1); 
-						double r2=2*Random(), dy=r2<1 ? sqrt(r2)-1: 1-sqrt(2-r2); 
-						Vector d = cx*( ( (sx+0.5 + dx)/Pixel + x)/w - 0.5) + cy*( ( (sy+0.5 + dy)/Pixel + y)/h - 0.5) + camera.Direction(); 
+					{
+						double r1=2*Random(), dx=r1<1 ? sqrt(r1)-1: 1-sqrt(2-r1);
+						double r2=2*Random(), dy=r2<1 ? sqrt(r2)-1: 1-sqrt(2-r2);
+						Vector d = cx*( ( (sx+0.5 + dx)/Pixel + x)/w - 0.5) + cy*( ( (sy+0.5 + dy)/Pixel + y)/h - 0.5) + camera.Direction();
 						//get color of objet hit by the ray
-						r = r + radiance(Ray(camera.Origin()+d*140,Normalized(d)),0)*(1./samps); 
+						r = r + radiance(Ray(camera.Origin()+d*140,Normalized(d)),0)*(1./samps);
 					}
 					//fill the  pixel with color
-					c[i] = c[i] + Vector(clamp(r[0]),clamp(r[1]),clamp(r[2]))*(1.0/((float) (Pixel*Pixel))); 
-				} 
-			} 
+					c[i] = c[i] + Vector(clamp(r[0]),clamp(r[1]),clamp(r[2]))*(1.0/((float) (Pixel*Pixel)));
+				}
+			}
 		}
 	}
 
 	// Save image as PPM file
-	FILE *f = fopen("image.ppm", "w"); 
-	fprintf(f, "P3\n%d %d\n%d\n", w, h, 255); 
-	for (int i=0; i<w*h; i++) 
+	FILE *f = fopen("image.ppm", "w");
+	fprintf(f, "P3\n%d %d\n%d\n", w, h, 255);
+	for (int i=0; i<w*h; i++)
 	{
 		fprintf(f,"%d %d %d ", toInt(c[i][0]), toInt(c[i][1]), toInt(c[i][2]));
 	}
 	fclose(f);
 	delete []c;
-	
+
 	t = now() - t;
 
 	fprintf(stderr, "\nProcessus used: %d", omp_get_num_procs());
