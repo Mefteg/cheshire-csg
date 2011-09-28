9414a623f26b244e0da487745f369b945a010344
pandavigoureux29@gmail.com <pandavigoureux29@gmail.com@3040dc66-f2c5-6624-7679-62bdbddada0d>
2011-09-28 14:47:24 +0000

    Translation ajoutée mais non fonctionnelles

---
+++ trunk/sources/translation.cpp
@@ -0,0 +1,33 @@
+#include "translation.h"
+
+Translation::Translation(void)
+{
+}
+
+Translation::Translation(Node * left,const Vector v) : Transfo(left)
+{
+	m(0,3) = v[0];
+	m(1,3) = v[1];
+	m(2,3) = v[2];
+}
+
+Translation::~Translation(void)
+{
+	delete left;
+}
+
+int Translation::Intersect(const Ray& ray, Intersection& t) {
+	Intersection tt;
+
+	Matrix4Df mInv = m.Invert(m);
+	
+	Ray r = Ray( mInv.MulPt(Vector(ray.Origin()[0],ray.Origin()[1],ray.Origin()[2])), ray.Direction());
+
+	if(left->Intersect(r,tt)){
+		t=tt;
+		t.obj = this->left;
+		return 1;
+	}
+	
+	return 0;
+}
