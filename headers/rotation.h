84aa89574a478fae778d1b1200874a7a07787040
pandavigoureux29@gmail.com <pandavigoureux29@gmail.com@3040dc66-f2c5-6624-7679-62bdbddada0d>
2011-10-02 17:27:41 +0000

    quelques reglages pour les transfos

--- headers/rotation.h
+++ headers/rotation.h
@@ -62,7 +62,7 @@ class Rotation : public Transfo
 
 	Vector getEmission() { return Vector(); };
 	Vector getColor() { return Vector(); };
-	Vector getPosition() { return Vector(); };
+	Vector getPosition() { return m.MulPt( left->getPosition()); };
 	int getRefl() { return 0; };
 	double getF() { return 0; };
 
